75cad28c7fc8313a1524cc857b25a77a6d89f57b
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-07-12 17:03:52 +0300

    Dark-mode

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const dropDown = document.querySelector(".dropdownMenu");
 const dropOptions = document.querySelector(".drop-options");
+const toggle = document.querySelector(".toggle");
+const icon = document.querySelector(".bx");
+
+toggle.addEventListener("click", (e) => {
+  document.body.classList.toggle("dark-mode");
+  toggle.classList.toggle("dark-mode");
+  icon.classList.toggle("bxs-moon");
+  dropDown.classList.toggle("dark-mode");
+});
 
 dropDown.addEventListener("click", (e) => {
   dropOptions.classList.toggle("show-options");
