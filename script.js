e5ec733b1e8a8325519f0e12a453d7a93f90c617
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-07-13 23:59:18 +0300

    Display Countries

--- script.js
+++ script.js
@@ -2,13 +2,6 @@ const dropDown = document.querySelector(".dropdownMenu");
 const dropOptions = document.querySelector(".drop-options");
 const toggle = document.querySelector(".toggle");
 const icon = document.querySelector(".bx");
-const country = document.querySelector(".country");
-
-async function getCountries() {
-  const URL = await fetch("https://restcountries.com/v2/all");
-  const res = wait URL.json();
-  console.log(res);
-}
 
 toggle.addEventListener("click", (e) => {
   document.body.classList.toggle("dark-mode");
@@ -20,3 +13,40 @@ toggle.addEventListener("click", (e) => {
 dropDown.addEventListener("click", (e) => {
   dropOptions.classList.toggle("show-options");
 });
+
+async function getCountries() {
+  const URL = await fetch("https://restcountries.com/v2/all");
+  const res = await URL.json();
+  console.log(res);
+  res.forEach((api) => {
+    showCountry(api);
+  });
+}
+
+getCountries();
+
+function showCountry(data) {
+  const country = document.createElement("div");
+  country.classList.add("country");
+
+  const language = data.languages
+    ? data.languages[Object.keys(data.languages)[0]]
+    : "No language";
+  const currency = data.currencies
+    ? data.currencies[Object.keys(data.currencies)[0]]?.name
+    : "No currency";
+  country.innerHTML = `<div class="country-img">
+          <img
+            src=${data.flag}
+            alt=""
+          />
+        </div>
+        <div class="country-details">
+          <h5>${data.name}</h5>
+          <p><strong>Population 👫:</strong>${data.population}</p>
+          <p><strong>Region 🌇:</strong>${data.region}</p>
+          <p><strong>Capital 🏙️:</strong>${data.capital}</p>
+          <p><strong>Languages 🗣️:</strong>${language}</p>
+          <p><strong>Languages 💰:</strong>${currency}</p>
+        </div>`;
+}
