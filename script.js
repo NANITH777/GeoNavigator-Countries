a50a05b4d32c79d811a79b1916090e35b9cd0451
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-07-13 23:27:47 +0300

    display countries

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const dropDown = document.querySelector(".dropdownMenu");
 const dropOptions = document.querySelector(".drop-options");
 const toggle = document.querySelector(".toggle");
 const icon = document.querySelector(".bx");
+const country = document.querySelector(".country");
+
+async function getCountries() {
+  const URL = await fetch("https://restcountries.com/v2/all");
+  const res = wait URL.json();
+  console.log(res);
+}
 
 toggle.addEventListener("click", (e) => {
   document.body.classList.toggle("dark-mode");
